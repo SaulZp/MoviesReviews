a5d48f81b42e51aba99bcc5b9166919e0ea3df4d
cinthia11 <43801911+cinthia11@users.noreply.github.com>
2019-04-24 14:06:28 -0500

    Add files via upload

---
+++ simbolos/footer1.php
@@ -0,0 +1,8 @@
+<!-- PIE DE PAGINA -->
+<footer class="bg-dark footer d-flex align-items-center"> 
+<div class="container d-md-flex d-block justify-content-between align-items-center">
+            <div class="text-white">
+                <p class="m-0">MoviesReviews 2019. Todos los derechos reservados &copy;</p>
+            </div>
+        </div><!--container-->
+  </footer><!--footer-->
